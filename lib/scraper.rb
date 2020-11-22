1cd116135d513623dba8459981d94adea3986dd6
Zachary Durr <zdurr22@gmail.com>
2020-11-22 23:46:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,6 +23,6 @@ class Scraper
   end
 
   def get_page
-    page = Nokogiri::HTML(open(http://learn-co-curriculum.github.io/site-for-scraping/courses))
+    page = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 end
