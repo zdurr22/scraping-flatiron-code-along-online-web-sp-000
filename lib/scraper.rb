fe125dc88a812b7e72e55a5bd1526fd084060c1b
Zachary Durr <zdurr22@gmail.com>
2020-11-22 23:55:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,8 +27,6 @@ class Scraper
     new_course.title = course.css("h2").text
     new_course.schedule = course.css("date").text
     new_course.description = course.css("p").text
-
-
   end
 
   def get_page
