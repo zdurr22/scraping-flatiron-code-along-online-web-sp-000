6e676bc587d5757a417838a89524e51eb1b48de1
Zachary Durr <zdurr22@gmail.com>
2020-11-22 23:42:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -19,6 +19,3 @@ class Scraper
   def get_courses
   end
 end
-
-
-
